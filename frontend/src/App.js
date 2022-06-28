61b449b6fdb2815b61cda06a400e3a10f41b7024
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-06-28 23:41:59 +0530

    adding components of product and rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import data from './data';
+import {Product} from './components/Product'
 
 function App() {
   return (
@@ -16,24 +17,8 @@ function App() {
     <main>
     <div className="row center"> 
       {
-        data.products.map((ele)=>(
-          <div key={ele._id} className="card">
-          <a href={`/product/${ele._id}`}>
-              <img className="medium" src={ele.image} alt={ele.name}/>
-          </a>
-          <div className="card-body">
-          <a href={`/product/${ele._id}`}>
-         <h2>{ele.name}</h2></a>
-       <div className="rating">
-       <span><i className="fa fa-star"></i></span>
-       <span><i className="fa fa-star"></i></span>
-       <span><i className="fa fa-star"></i></span>
-       <span><i className="fa fa-star"></i></span>
-       <span><i className="fa fa-star-o"></i></span>
-       </div>
-       <div className="price">₹{ele.price}</div>
-          </div>
-           </div>
+        data.products.map((product)=>(
+        <Product key={product._id} product={product}></Product>
         ))
       }
 
